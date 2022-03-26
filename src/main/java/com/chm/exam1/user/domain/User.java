277a9e4368f27fb9b762cb71f82353f494c21c64
goodmean <zizigoo12@gmail.com>
2022-03-26 16:18:14 +0900

    jpa 테스트, lombok 적용

--- src/main/java/com/chm/exam1/user/domain/User.java
+++ src/main/java/com/chm/exam1/user/domain/User.java
@@ -1,13 +1,22 @@
 package com.chm.exam1.user.domain;
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
+@Entity
+@Getter
 public class User {
-	public long id;
-	public LocalDateTime regDate;
-	public LocalDateTime updateDate;
-	public String email;
-    public String password;
-    public String name;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private LocalDateTime regDate;
+	private LocalDateTime updateDate;
+	private String email;
+	private String password;
+	private String name;
 }
